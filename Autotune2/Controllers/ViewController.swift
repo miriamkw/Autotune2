d45e04dbb3f5746fabe5c4449e63f9e92a4a9b46
Miriam K. Wolff <miriamkwolff@hotmail.com>
2022-09-06 12:31:37 +0200

    Added user profile details and display them

--- Autotune2/Controllers/ViewController.swift
+++ Autotune2/Controllers/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
 
+    private let userHealthProfile = UserHealthProfile()
+    
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var biologicalSexLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadAndDisplayAgeAndSex()
     }
+    
+    private func loadAndDisplayAgeAndSex() {
+        do {
+          let userAgeAndSex = try ProfileDataStore.getAgeAndSex()
+          userHealthProfile.age = userAgeAndSex.age
+          userHealthProfile.biologicalSex =  userAgeAndSex.biologicalSex
+          updateLabels()
+        } catch let error {
+          print("Error loading user profile details \(error)")
+        }
+    }
+    
+    private func updateLabels() {
+        if let age = userHealthProfile.age {
+          ageLabel.text = "\(age)"
+        }
 
-
+        if let biologicalSex = userHealthProfile.biologicalSex {
+            biologicalSexLabel.text = biologicalSex.stringRepresentation
+        }
+    }
+    
 }
 
---
+++ Autotune2/Supporting Files/ProfileDataStore.swift
@@ -0,0 +1,64 @@
+/**
+ * This file is borrowed from the following free of charge HealthKit Tutorial:
+ * https://www.raywenderlich.com/459-healthkit-tutorial-with-swift-getting-started#ratings-count-hook
+ *
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import HealthKit
+
+class ProfileDataStore {
+    
+    // Asks for age (calculated from date), sex and blood type
+    class func getAgeAndSex() throws -> (
+        age: Int,
+        biologicalSex: HKBiologicalSex) {
+        
+      let healthKitStore = HKHealthStore()
+        
+      do {
+        //1. This method throws an error if these data are not available.
+        let birthdayComponents =  try healthKitStore.dateOfBirthComponents()
+        let biologicalSex =       try healthKitStore.biologicalSex()
+          
+        //2. Use Calendar to calculate age.
+        let today = Date()
+        let calendar = Calendar.current
+        let todayDateComponents = calendar.dateComponents([.year],
+                                                            from: today)
+        let thisYear = todayDateComponents.year!
+        let age = thisYear - birthdayComponents.year!
+         
+        //3. Unwrap the wrappers to get the underlying enum values.
+        let unwrappedBiologicalSex = biologicalSex.biologicalSex
+          
+        return (age, unwrappedBiologicalSex)
+      }
+    }
+}
